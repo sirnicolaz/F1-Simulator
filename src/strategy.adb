b4aa8b19378670ab7d4b7e4f2a2b20747ac9d0e2
ulcera <devnull@localhost>
2009-09-29 17:22:23 +0200

    strategy updated

--- src/strategy.adb
+++ src/strategy.adb
@@ -1,3 +1,50 @@
+with Circuit;
+use Circuit;
+with Competitor;
+use Competitor;
+with Competitor;
+
+with Ada.Calendar;
+use Ada.Calendar;
+with Ada.Real_Time;
+use Ada.Real_Time;
+
 package body Strategy is
 
-end Strategy
+   function evaluate(segm : SEGMENT;
+                     driver : comp) return Ada.Calendar.TIME is
+
+   begin
+      --qua dentro va effettuata la valutazione della traiettoria migliore e calcolato il tempo di attraversamento
+      -- da restituire poi a chi invoca questo metodo.
+      --qua credo che vadano eseguite le operazioni per attraversare il tratto
+
+   end evaluate;
+
+
+   function Get_pitstopGasolineLevel(str_In : STRATEGY) return INTEGER is
+   begin
+      return str_In.pitstopGasolineLevel;
+   end Get_pitstopGasolineLevel;
+
+   function Get_pitstopLaps(str_In : STRATEGY) return INTEGER is
+   begin
+      return str_In.pitstopLaps;
+   end Get_pitstopLaps;
+
+   function Get_pitstopCondition (str_In : STRATEGY) return BOOLEAN is
+   begin
+      return str_In.pitstopCondition;
+   end Get_pitstopCondition;
+
+   function Get_trim (str_In : STRATEGY) return INTEGER is
+   begin
+      return str_In.trim;
+   end Get_trim;
+
+   function Get_pitstop (str_In : STRATEGY) return BOOLEAN is
+   begin
+      return str_In.pitstop;
+   end Get_pitstop;
+
+end Strategy;
--- src/strategy.ads
+++ src/strategy.ads
@@ -1,14 +1,25 @@
 with Circuit;
 use Circuit;
+with Competitor;
+use Competitor;
+with Competitor;
+
 with Ada.Calendar;
+use Ada.Calendar;
+with Ada.Real_Time;
+use Ada.Real_Time;
+
 package Strategy is
-   type STRATEGY is private
+   subtype comp is Competitor.CAR;
+   function evaluate(segm : SEGMENT;
+                     driver : comp) return Ada.Calendar.TIME;
+   type STRATEGY is
       record
-         pitstopGasolineLevel := INTEGER;
-         pitstopLaps := INTEGER;
-         pitstopCondition := BOOLEAN; -- correggere il tipo
-         trim := INTEGER; -- correggere il tipo
-         pitstop := BOOLEAN;
+         pitstopGasolineLevel : INTEGER;
+         pitstopLaps : INTEGER;
+         pitstopCondition : BOOLEAN; -- correggere il tipo
+         trim : INTEGER; -- correggere il tipo
+         pitstop : BOOLEAN;
       end record;
-      function evaluate(segm : SEGMENT) return TIME;
-end Strategy
+
+end Strategy;
