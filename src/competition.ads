72468273f0e4465a11382ba32bb8bcdb05ad1abc
jack <jack@jacktop>
2009-12-15 00:24:04 +0100

    Aggiungo un file di progettazione della procedura di inizio gara

---
+++ src/competition.ads
@@ -0,0 +1,17 @@
+with Circuit;
+use Circuit;
+
+package Competition is
+
+   procedure Configure_Circuit( ClassificRefreshRate_In : FLOAT;
+                               CircuitName_In : STRING;
+                               CircuitLocation_In : STRING;
+                               RaceConfigFile_In : STRING );
+
+   procedure Configure_Ride( LapsQty_In : INTEGER );
+   procedure Join(
+                  Car_In : ;
+                  Strategy_In ;
+                  CompetitorInfo;
+
+end Competition;
