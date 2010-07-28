a7cf21edb105cd5c07eee14232733940ce9fbf4f
ulcera <devnull@localhost>
2010-07-28 10:22:03 +0200

    ho aggiunto il file test.adb che è il main che ho usato io di prova per le statistiche.

---
+++ src/test.adb
@@ -0,0 +1,71 @@
+with System;
+with Ada.Text_IO;
+use Ada.Text_IO;
+with Ada.Float_Text_IO;
+with Ada.Calendar;
+--  with Competitor;
+--  use Competitor;
+--with Competition;
+--use Competition;
+with Circuit;
+use Circuit;
+with Input_Sources.File;
+use Input_Sources.File;
+with Sax.Readers; use Sax.Readers;
+with DOM.Readers; use DOM.Readers;
+with DOM.Core; use DOM.Core;
+with DOM.Core.Documents; use DOM.Core.Documents;
+with DOM.Core.Nodes; use DOM.Core.Nodes;
+with DOM.Core.Attrs; use DOM.Core.Attrs;
+with Ada.IO_Exceptions;
+with OnBoardComputer;
+use OnBoardComputer;
+with Common;
+use Common;
+  with Stats;
+  use Stats;
+
+procedure Test is
+   RaceTrackTemp : Circuit.RACETRACK_POINT :=Get_Racetrack("./obj/racetrack.xml");
+   obc : COMPUTER_POINT := new COMPUTER;
+   compStats : COMP_STATS_POINT := new COMP_STATS;
+   predictedTime : FLOAT := 150.0;
+   upd : FLOAT := 100.0;
+   global : GLOBAL_STATS_HANDLER_POINT;
+   temp : GENERIC_STATS_POINT := new GENERIC_STATS;
+   temp_2 : GENERIC_STATS_POINT;
+   temp_new : GENERIC_STATS_POINT := new GENERIC_STATS;
+begin
+   --temp := new GENERIC_STATS_POINT;
+   temp_2 := temp;
+   global := new GLOBAL_STATS_HANDLER(new FLOAT'(upd), temp);--new GENERIC_STATS);--'(temp.all));
+                                                     --pritnGlobUPD(global.all);
+   setGSLAP(temp_new.all, 1, 42, 10.0);
+   setGS_SECTOR(temp_new.all,1, 1, 42, 0.3);
+   setGS_SECTOR(temp_new.all,2, 1, 42, 0.3);
+   setGS_SECTOR(temp_new.all,3, 1, 42, 0.3);
+   Ada.Text_IO.Put_Line("global.updatePeriod : "&Float'Image(global.updatePeriod));
+   --AGGIORNAMENTO ONBOARDCOMPUTER
+         -- qua va creata la statistica da aggiungere al computer di bordo
+    -- per poi invocare il metodo Add_Data
+  -- Ada.Text_IO.Put_Line("PRE setcheckpoint");
+   Common.Set_Checkpoint(compStats, 1);
+   -- Ada.Text_IO.Put_Line("POST setcheckpoint PRE setSector");
+   Common.Set_Sector(compStats, 1); -- TODO, non abbiamo definito i sector, ritorna sempre uno.
+   -- ONBOARDCOMPUTER.Set_Lap(); -- TODO, non ho ancora un modo per sapere il numero di giro
+                                                   --commentato- da correggere il ripo di gaslevel ONBOARDCOMPUTER.Set_Gas(compStats, carDriver.auto.GasolineLevel);
+   -- Ada.Text_IO.Put_Line("POST setsector PRE set tyre");
+   Common.Set_Tyre(compStats, 0.15);
+   -- Ada.Text_IO.Put_Line("POST settyre PREset time");
+   Common.Set_Time(compStats, predictedTime);
+   --se il tempo segnato è tale da inserire il nuovo valore nella tabella lo inserisco.
+  global.global.Init_GlobalStats(temp_2,10, upd);
+   if predictedTime >100.0 then
+      Ada.Text_IO.Put_Line("predictedTime > 100.0");
+      -- Common.Update_Stats(compStats);--, global);
+      updateCompetitorInfo(global, 1, compStats);
+   end if;
+  compareTime(temp_new.all, temp_2.all);--passaggio primo parametro mancante
+   obc.Add_Data(compStats);
+         --FINE AGGIORNAMENTO ONBOARDCOMPUTER
+end Test;
