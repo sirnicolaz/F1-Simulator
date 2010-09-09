8ffc5a9c9d319bf7d53824b980f07ea5ea4560f7
ulcera <devnull@localhost>
2010-09-09 15:08:46 +0200

    modifiche agli script e alla relazione

--- install-competition.sh
+++ install-competition.sh
@@ -2,9 +2,14 @@
 
 echo "Compiling competition server"
 cd src/ada/
+idlac ../idl/radio.idl 
+idlac ../idl/init.idl
+rm corba.ads
 make competition
 
 echo "Compiling competition screen"
 cd ../java
+idlj ../idl/radio.idl;
+idlj ../idl/init.idl;
 make competition
 
